0e289e8d2bd6eb382f3d929b2c1f900a40f568a9
d-golman <danya1golman@gmail.com>
2020-09-09 20:54:30 +0700

    bot init

---
+++ bot/main.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import InlineQuery, InputTextMessageContent, InlineQueryResultArticle
+import aiohttp
+import os
+import config
+
+load_dotenv('.env')
+TOKEN = os.getenv('TOKEN')
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+config.step = 0
+
+@dp.message_handler(commands = ['start'])
+async def startMessage(message):    
+    menu = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True, one_time_keyboard=False)
+    btn1 = types.KeyboardButton('О вузе')
+    menu.row(btn1)
+    await bot.send_message(message.chat.id,'Хуюзе',reply_markup=menu)
+    config.step = 0
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
